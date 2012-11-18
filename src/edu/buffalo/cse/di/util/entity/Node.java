691e15245f608e43dcbf42622b804c4e4fdb0bbe
Sravan <sravanreddy001@gmail.com>
2012-11-18 18:27:36 -0500

    Added additional attributes used for similarity Score calculation

--- src/edu/buffalo/cse/di/util/entity/Node.java
+++ src/edu/buffalo/cse/di/util/entity/Node.java
@@ -3,6 +3,8 @@
  */
 package edu.buffalo.cse.di.util.entity;
 
+import java.util.List;
+
 /**
  * Node for the KNN Algorithm
  * @author sravanku@buffalo.edu
@@ -10,17 +12,54 @@ package edu.buffalo.cse.di.util.entity;
 public class Node {
     // TODO This very basic representation has only a single attribute.
     private final String string;
+    private final List<String> urls;
+    private final List<String> titles;
+    private final String header;
+    private final String text;
+    
 
-    public Node(String string) {
+    public Node(String string, List<String> urls, List<String> titles,
+            String header, String text) {
+        
         this.string = string;
+        // Convert to lower case Strings
+        for(int i=0; i<urls.size(); i++) {
+            urls.set(i, urls.get(i).toLowerCase());
+        }        
+        for(int i=0; i<titles.size(); i++) {
+            titles.set(i, titles.get(i).toLowerCase());
+        }
+
+        this.urls   = urls;
+        this.titles = titles;
+        this.header = header;
+        this.text = text;
     }
     
     public String getString() {
         return string;
     }
 
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public List<String> getTitles() {
+        return titles;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getText() {
+        return text;
+    }
+
     @Override
     public String toString() {
-        return "Node [string=" + string + "]";
+        return "Node [string=" + string + ", urls=" + urls + ", titles="
+                + titles + ", header=" + header + ", text=" + text + "]";
     }
+
 }
