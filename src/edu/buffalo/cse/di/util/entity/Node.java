21c78c23eb0f9886329647aa6aa2f7e10b33d6b8
Sravan <sravanreddy001@gmail.com>
2012-11-19 00:36:27 -0500

    Null check handling for the url and title lists.

--- src/edu/buffalo/cse/di/util/entity/Node.java
+++ src/edu/buffalo/cse/di/util/entity/Node.java
@@ -3,6 +3,7 @@
  */
 package edu.buffalo.cse.di.util.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,11 +17,25 @@ public class Node {
     private final List<String> titles;
     private final String header;
     private final String text;
-    
 
+    /**
+     * Create a Node object
+     * @param string
+     * @param urls
+     * @param titles
+     * @param header
+     * @param text
+     */
     public Node(String string, List<String> urls, List<String> titles,
             String header, String text) {
-        
+        // Avoid null check at later calls when ever used.
+        if(urls == null) {
+            urls = new ArrayList<String>(0);
+        }
+        if(titles == null) {
+            titles = new ArrayList<String>(0);
+        }
+
         this.string = string;
         // Convert to lower case Strings
         for(int i=0; i<urls.size(); i++) {
@@ -35,7 +50,7 @@ public class Node {
         this.header = header;
         this.text = text;
     }
-    
+
     public String getString() {
         return string;
     }
