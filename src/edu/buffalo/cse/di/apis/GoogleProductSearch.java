38767b506069971c3bb06eb0133ca817ad20bee8
Sravan <sravanreddy001@gmail.com>
2012-12-01 16:39:11 -0500

    Adding Exception handling and "Printing the clusters.

--- src/edu/buffalo/cse/di/apis/GoogleProductSearch.java
+++ src/edu/buffalo/cse/di/apis/GoogleProductSearch.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -88,32 +89,37 @@ public class GoogleProductSearch extends GoogleSearch {
      */
     public static List<GoogleProductSearchResult> searchProducts(String query, List<String> ignoreList) {
         String content = queryGoogleProductSearch(query);
-
-        JSONObject obj = (JSONObject) JSONSerializer.toJSON(content);
-        JSONArray items = obj.getJSONArray("items");
         List<GoogleProductSearchResult> itemNames = new ArrayList<GoogleProductSearchResult>();
-        for(int i=0; i<items.size(); i++) {
-            JSONObject item = items.getJSONObject(i);
-            JSONObject product = item.getJSONObject("product");
+        try {
+            JSONObject obj = (JSONObject) JSONSerializer.toJSON(content);
+            JSONArray items = obj.getJSONArray("items");
+            
+            for(int i=0; i<items.size(); i++) {
+                JSONObject item = items.getJSONObject(i);
+                JSONObject product = item.getJSONObject("product");
 
-            //Filtered the titles to ignore 
-            String title = product.getString("title");
-            boolean flag = false;
-            if(ignoreList != null && ignoreList.size() != 0) {
-                for (String ignoreStr: ignoreList) {
-                    if (title.toLowerCase().contains(ignoreStr.toLowerCase())) {
-                        flag = true;
-                    }	
+                //Filtered the titles to ignore 
+                String title = product.getString("title");
+                boolean flag = false;
+                if(ignoreList != null && ignoreList.size() != 0) {
+                    for (String ignoreStr: ignoreList) {
+                        if (title.toLowerCase().contains(ignoreStr.toLowerCase())) {
+                            flag = true;
+                        }	
+                    }
                 }
-            }
 
-            if (!(flag)) {
-                GoogleProductSearchResult result = new GoogleProductSearchResult(title); 
-                itemNames.add(result);
+                if (!(flag)) {
+                    GoogleProductSearchResult result = new GoogleProductSearchResult(title); 
+                    itemNames.add(result);
+                }
+                //String title = 
+                //System.out.println(item.get("product"));
+                //System.out.println(result);
             }
-            //String title = 
-            //System.out.println(item.get("product"));
-            //System.out.println(result);
+        }
+        catch(Exception e) {
+            //TODO handle error.
         }
         return itemNames;
     }
@@ -134,7 +140,7 @@ public class GoogleProductSearch extends GoogleSearch {
         }
         return null;
     }
-    
+
     /**
      * This Method add's an additional functionality to automatically 
      * calculate metrics over the existing implmentation present in 
@@ -181,8 +187,8 @@ public class GoogleProductSearch extends GoogleSearch {
 
         List<Node> nodes = new ArrayList<Node>();
         for(GoogleProductSearchResult item: data) {
-         // Modified for calculation of classification rate.
-            Node node = getNodesForProductSearchModified(item); 
+            // TODO use "Modified" if required for calculation of classification rate.
+            Node node = getNodesForProductSearch(item); 
             if(node != null) {
                 nodes.add(node);
             }
@@ -192,15 +198,15 @@ public class GoogleProductSearch extends GoogleSearch {
         KNNAlgorithm algorithm = new KNNAlgorithm(nodes, 5, 0.15);
         List<List<Node>> clusters = algorithm.generateClusters(SimilarityType.JACCARD);
         System.out.println("Step 1: UnMerged Clusters: " + clusters.size());
-        
+
         List<List<Node>> mergedClusters = getMergedClusters(clusters,0.0);
-        
+
         System.out.println("Step 2: Merged Clusters: " + mergedClusters.size());
-        /*for(List<Node> cluster: mergedClusters) {
+        for(List<Node> cluster: mergedClusters) {
             System.out.println(cluster);
             //System.out.println(SimilarityScore.getBestNodeForCluster(cluster));
-        }*/
-        
+        }
+
         //Step 2:
         /*List<Node> headerNodes = new ArrayList<Node>();
         for(List<Node> cluster: clusters) {
@@ -212,65 +218,63 @@ public class GoogleProductSearch extends GoogleSearch {
         for(List<Node> cluster: clusters1) {
             System.out.println(cluster);
         }*/
-        
+
         return clusters;
     }
-    
+
     public static List<List<Node>> getMergedClusters(List<List<Node>> clusters, double threshold) {
-    	List<List<Node>> minClusters = new ArrayList<List<Node>>();
-    	List<List<Node>> maxClusters = new ArrayList<List<Node>>();
-    	List<Node> unClassifiedCluster = new ArrayList<Node>();
-    	
-    	for(List<Node> cluster: clusters) {
-    		if (cluster.size() < 3) 
-    			minClusters.add(cluster);
-    		else
-    			maxClusters.add(cluster);
-    	}
-    	
-    	List<Node> headerNodes = new ArrayList<Node>();
+        List<List<Node>> minClusters = new ArrayList<List<Node>>();
+        List<List<Node>> maxClusters = new ArrayList<List<Node>>();
+        List<Node> unClassifiedCluster = new ArrayList<Node>();
+
+        for(List<Node> cluster: clusters) {
+            if (cluster.size() < 3) 
+                minClusters.add(cluster);
+            else
+                maxClusters.add(cluster);
+        }
+        //System.out.println("Get ClusterHeads : ");
+        List<Node> headerNodes = new ArrayList<Node>();
         for(List<Node> cluster: maxClusters) {
             headerNodes.add(SimilarityScore.getBestNodeForCluster(cluster));
         }
-        
+
         //SimilarityScore simScore = new SimilarityScore();
         for(List<Node> minCluster: minClusters) {
-        	for(Node minNode: minCluster) {
-        		String minNodeName = minNode.getString();
-        		List<Node> minNodeNearestClstr = null;
-        		Node minNodeNearestNode = null;
-        		double minDist = 0.0;
-        		int index = 0;
-        		int count = 0;
-        		for(Node headerNode: headerNodes) {
-        			//double curSimScore = simScore.getJaccardSimilarty(minNodeName, headerNode.getString());
-        			double curDist = KNNAlgorithm.getDistanceBetweenNodes(headerNode, minNode, SimilarityType.JACCARD);
-        			if (minDist == 0.0) {
-        				minDist = curDist;
-        				minNodeNearestNode = headerNode;
-        				index = count;
-        			} else {
-        				if ((curDist > minDist) && (curDist > threshold)) {
-            				minDist = curDist;
-            				minNodeNearestNode = headerNode;
-            				index = count;
-            			}
-        			}
-        			count++;
-        		}
-        		
-        		if (minNodeNearestNode != null) {
-        			maxClusters.get(index).add(minNode);
-        			//maxClusters.remove(index);
-        			//cluster.add(minNode);
-        			System.out.println(minNodeNearestNode + "---> " + minNode);
-        			//maxClusters.add(cluster);
-        		} else {
-        			unClassifiedCluster.add(minNode);
-        		}
-        	}
+            for(Node minNode: minCluster) {
+                Node minNodeNearestNode = null;
+                double minDist = 0.0;
+                int index = 0;
+                int count = 0;
+                for(Node headerNode: headerNodes) {
+                    //double curSimScore = simScore.getJaccardSimilarty(minNodeName, headerNode.getString());
+                    double curDist = KNNAlgorithm.getDistanceBetweenNodes(headerNode, minNode, SimilarityType.JACCARD);
+                    if (minDist == 0.0) {
+                        minDist = curDist;
+                        minNodeNearestNode = headerNode;
+                        index = count;
+                    } else {
+                        if ((curDist > minDist) && (curDist > threshold)) {
+                            minDist = curDist;
+                            minNodeNearestNode = headerNode;
+                            index = count;
+                        }
+                    }
+                    count++;
+                }
+
+                if (minNodeNearestNode != null) {
+                    maxClusters.get(index).add(minNode);
+                    //maxClusters.remove(index);
+                    //cluster.add(minNode);
+                    //System.out.println(minNodeNearestNode + "---> " + minNode);
+                    //maxClusters.add(cluster);
+                } else {
+                    unClassifiedCluster.add(minNode);
+                }
+            }
         }
-        
+
         maxClusters.add(unClassifiedCluster);
         return maxClusters;
     }
@@ -278,8 +282,9 @@ public class GoogleProductSearch extends GoogleSearch {
 
     public static void main(String[] args) throws IOException {
         //actualTest("MobilePhonesFiltered.txt");
-        actualTestModified("MobilePhonesWithLabels.txt");
-        //String[] ignoreList = new String[] {"case","Charger"};
+        actualTestModified("TelevisionDataSet.txt");
+        //String[] ignoreListForMobile = new String[] {"case","Charger"};
+        //testData(null);
         //testData(Arrays.asList(ignoreList));
     }
 
@@ -296,7 +301,7 @@ public class GoogleProductSearch extends GoogleSearch {
         reader.close();
         runAlgorithmforStrings(productTitles);
     }
-    
+
     /**
      * Automation of classification rate calculation
      * @param fileName
@@ -315,9 +320,9 @@ public class GoogleProductSearch extends GoogleSearch {
         }
         reader.close();
         List<List<Node>> clusters = runAlgorithmforStrings(productTitles);
-        calculateClassificationRate(clusters);
+        //calculateClassificationRate(clusters);
     }
-    
+
     public static void calculateClassificationRate(List<List<Node>> clusters) {
         int totalRecords = 0;
         int correctClassified = 0;
@@ -341,11 +346,12 @@ public class GoogleProductSearch extends GoogleSearch {
         }
         System.out.println("(Total Nodes, Correct Classified = (" + totalRecords + ", " + correctClassified + ")");
     }
-    
+
     public static void testData(List<String> ignoreList) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("MobilePhonesSample.txt")));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("SampleTVList.txt")));
         String line = null;
+        
         while((line = reader.readLine()) != null) {
             if(!line.equals("")) {
                 System.out.println("---------------------------------------------------------------");
